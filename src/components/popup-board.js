0bd085a1ae9d004ed07f18c00f133801a8349e59
Maksim Baravy <35226420+maximkatut@users.noreply.github.com>
2020-04-04 00:44:15 -0400

    create components for each functional parton site, add imports to main.js

---
+++ src/components/popup-board.js
@@ -0,0 +1,12 @@
+export const createPopupBoardTemplate = () => {
+  return (`
+    <section class="film-details">
+      <form class="film-details__inner" action="" method="get">
+        <div class="form-details__top-container">
+          <div class="film-details__close">
+            <button class="film-details__close-btn" type="button">close</button>
+          </div>
+        </div>
+      </form>
+    </section>`);
+};
