e177e066466ae783ebc6fe035f2a3702b1ec7e62
Maksim Baravy <35226420+maximkatut@users.noreply.github.com>
2020-05-07 13:11:53 -0400

    refactoring of popup-controller depending on popup-controls-component

--- src/controllers/popup-controller.js
+++ src/controllers/popup-controller.js
@@ -1,5 +1,6 @@
-import PopupCommentsComponent from "../components/popup-comments.js";
 import PopupComponent from "../components/popup.js";
+import PopupControlsComponent from "../components/popup-controls.js";
+import PopupCommentsComponent from "../components/popup-comments.js";
 
 import {RenderPosition, render, remove, replace} from "../utils/render.js";
 
@@ -11,6 +12,7 @@ export default class PopupController {
     this._onViewChange = onViewChange;
 
     this._popupComponent = null;
+    this._popupControlsComponent = null;
     this._popupCommentsComponent = null;
 
     this._onKeyDown = this._onKeyDown.bind(this);
@@ -22,35 +24,43 @@ export default class PopupController {
   render(card) {
     this._card = card;
     this._onViewChange();
-    const oldPopupComponent = this._popupComponent;
     // Find body element for rendering popup card
     const siteBodyElement = document.querySelector(`body`);
-    this._popupComponent = new PopupComponent(this._card);
-    this._popupCommentsComponent = new PopupCommentsComponent(this._card.comments);
-    if (oldPopupComponent) {
-      replace(this._popupComponent, oldPopupComponent);
+    const oldPopupControlsComponent = this._popupControlsComponent;
+
+    this._popupControlsComponent = new PopupControlsComponent(this._card);
+
+    if (oldPopupControlsComponent) {
+      replace(this._popupControlsComponent, oldPopupControlsComponent);
     } else {
+      this._popupComponent = new PopupComponent(this._card);
+      this._popupCommentsComponent = new PopupCommentsComponent(this._card.comments);
       render(siteBodyElement, this._popupComponent, RenderPosition.BEFOREEND);
+      render(this._popupComponent.getPopupControlsContainer(), this._popupControlsComponent, RenderPosition.BEFOREEND);
       render(this._popupComponent.getPopupCommentsContainer(), this._popupCommentsComponent, RenderPosition.BEFOREEND);
     }
+
     // set click event for popup close button and Esc key
     this._popupComponent.setClosePopupClickHandler(this._onCloseButtonClick);
 
-    this._popupComponent.setWatchlistClickHandler(() => {
+    this._popupControlsComponent.setWatchlistClickHandler(() => {
       this._onDataChange(this._card, Object.assign({}, this._card, {
         isInWatchlist: !this._card.isInWatchlist
       }));
     });
-    this._popupComponent.setWatchedClickHandler(() => {
+
+    this._popupControlsComponent.setWatchedClickHandler(() => {
       this._onDataChange(this._card, Object.assign({}, this._card, {
         isWatched: !this._card.isWatched
       }));
     });
-    this._popupComponent.setFavoriteClickHandler(() => {
+
+    this._popupControlsComponent.setFavoriteClickHandler(() => {
       this._onDataChange(this._card, Object.assign({}, this._card, {
         isFavorite: !this._card.isFavorite
       }));
     });
+
     this._popupCommentsComponent.setChangeEmojiClickHandler((evt) => {
       if (evt.target.tagName !== `INPUT`) {
         return;
@@ -73,8 +83,8 @@ export default class PopupController {
     const isEscapeKey = evt.key === `Esc` || evt.key === `Escape`;
     if (isEscapeKey) {
       remove(this._popupComponent);
+      document.removeEventListener(`keydown`, this._onKeyDown);
     }
-    document.removeEventListener(`keydown`, this._onKeyDown);
   }
 
   destroy() {
