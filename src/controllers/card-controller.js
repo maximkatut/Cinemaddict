708508b01f8b5966d0ba602b367d7ec4ede7ff53
Maksim Baravy <35226420+maximkatut@users.noreply.github.com>
2020-05-12 10:59:21 -0400

    refactoring of card-component and card-controller

--- src/controllers/card-controller.js
+++ src/controllers/card-controller.js
@@ -1,7 +1,8 @@
 import CardComponent from "../components/card.js";
+import CardControlsComponent from "../components/card-controls.js";
 import PopupController from "../controllers/popup-controller.js";
 
-import {RenderPosition, render, remove, replace} from "../utils/render.js";
+import {RenderPosition, render, remove} from "../utils/render.js";
 
 export default class CardController {
   constructor(container, onDataChange, onViewChange) {
@@ -11,6 +12,7 @@ export default class CardController {
 
     this._card = {};
     this._cardComponent = null;
+    this._cardControlsComponent = null;
 
     this._commentsModel = null;
     this._popupController = null;
@@ -18,48 +20,66 @@ export default class CardController {
     this._showPopup = this._showPopup.bind(this);
   }
 
-  _initCardComponent() {
-    // Rendering the actual card, if card allready exists than replace it
-    const oldCardComponent = this._cardComponent;
-    this._cardComponent = new CardComponent(this._card);
-    if (oldCardComponent) {
-      replace(this._cardComponent, oldCardComponent);
-    } else {
-      render(this._container, this._cardComponent, RenderPosition.BEFOREEND);
-    }
-    // Add listeners for poster, name and comments to open popup
-    this._cardComponent.setOpenPopupClickHandler(this._showPopup);
+  render(card) {
+    const oldCard = this._card;
+    this._card = card;
+    if (this._cardComponent) {
+      if (this._popupController) {
+        this._popupController.render(this._card);
+      }
+      let isCardControlsChanged = false;
+      let isOtherFieldsChanged = false;
+      const controlsKeys = [`isFavorite`, `isInWatchlist`, `isWatched`];
+      Object.keys(card).forEach((key) => {
+        if (oldCard[key] !== this._card[key]) {
+          if (controlsKeys.includes(key)) {
+            isCardControlsChanged = true;
+          } else {
+            isOtherFieldsChanged = true;
+          }
+        }
+      });
+      const isOnlyCardControlsChanged = isCardControlsChanged && !isOtherFieldsChanged;
+      if (isOnlyCardControlsChanged) {
+        this._cardControlsComponent.rerender(this._card);
+        return;
+      }
 
-    this._cardComponent.setWatchlistClickHandler((evt) => {
+      this._cardComponent.rerender(this._card);
+      this._cardControlsComponent = new CardControlsComponent(this._card);
+      render(this._cardComponent.getElement(), this._cardControlsComponent, RenderPosition.BEFOREEND);
+      this._addEventListenersToCardControls();
+      return;
+    }
+
+    this._cardComponent = new CardComponent(this._card);
+    this._cardControlsComponent = new CardControlsComponent(this._card);
+    render(this._container, this._cardComponent, RenderPosition.BEFOREEND);
+    render(this._cardComponent.getElement(), this._cardControlsComponent, RenderPosition.BEFOREEND);
+
+    this._cardComponent.setOpenPopupClickHandler(this._showPopup);
+    this._addEventListenersToCardControls();
+  }
+
+  _addEventListenersToCardControls() {
+    this._cardControlsComponent.setWatchlistClickHandler((evt) => {
       evt.preventDefault();
       this._onDataChange(this._card, Object.assign({}, this._card, {
         isInWatchlist: !this._card.isInWatchlist
       }));
     });
-    this._cardComponent.setWatchedClickHandler((evt) => {
+    this._cardControlsComponent.setWatchedClickHandler((evt) => {
       evt.preventDefault();
       this._onDataChange(this._card, Object.assign({}, this._card, {
         isWatched: !this._card.isWatched
       }));
     });
-    this._cardComponent.setFavoriteClickHandler((evt) => {
+    this._cardControlsComponent.setFavoriteClickHandler((evt) => {
       evt.preventDefault();
       this._onDataChange(this._card, Object.assign({}, this._card, {
         isFavorite: !this._card.isFavorite
       }));
     });
-  }
-
-  render(card) {
-    this._card = card;
-    if (this._cardComponent) {
-      this._cardComponent.update(this._card);
-      if (this._popupController) {
-        this._popupController.render(this._card);
-      }
-    } else {
-      this._initCardComponent();
-    }
   }
 
   _showPopup() {
