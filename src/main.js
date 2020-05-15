1bc3916777440505793c927a2478c8efc0f81163
Maksim Baravy <35226420+maximkatut@users.noreply.github.com>
2020-05-15 15:58:14 -0400

    refactoring

--- src/main.js
+++ src/main.js
@@ -1,28 +1,27 @@
-import ProfileComponent from "./components/profile.js";
-import FilmsBoardComponent from "./components/films-board.js";
 import PageController from "./controllers/page-controller.js";
-import FilmsCountComponent from "./components/films-count.js";
-import StatisticsComponent from "./components/statistics.js";
+import FilterController from "./controllers/filter-controller.js";
+import ProfileComponent from "./components/profile.js";
 import MainNavigationComponent from "./components/main-navigation.js";
+import FilmsBoardComponent from "./components/films-board.js";
+import StatisticsComponent from "./components/statistics.js";
+import FilmsCountComponent from "./components/films-count.js";
 import CardsModel from './models/cards.js';
-import {generateCards} from "./mock/card.js";
+import {ActiveScreen, FilterType} from "./const.js";
 import {RenderPosition, render} from "./utils/render.js";
 import {getCardsByFilter} from "./utils/filter.js";
-import FilterController from "./controllers/filter-controller.js";
-import {ActiveScreen, FilterType} from "./const.js";
+import {generateCards} from "./mock/card.js";
 
-// CONSTANTS
 const CARDS_COUNT = 25;
 
-// Variables
 const cards = generateCards(CARDS_COUNT);
+const cardsModel = new CardsModel();
+cardsModel.setCards(cards);
+const watchedMovies = getCardsByFilter(FilterType.HISTORY, cardsModel.getCardsAll()).length;
 
 const siteHeaderElement = document.querySelector(`.header`);
 const siteMainElement = document.querySelector(`.main`);
 const siteFooterElement = document.querySelector(`.footer`);
-
-const cardsModel = new CardsModel();
-cardsModel.setCards(cards);
+const siteCountStatisticsElement = siteFooterElement.querySelector(`.footer__statistics`);
 
 const onScreenChangeHandler = (activeScreen) => {
   switch (activeScreen) {
@@ -39,31 +38,20 @@ const onScreenChangeHandler = (activeScreen) => {
   }
 };
 
+const profileComponent = new ProfileComponent(watchedMovies);
 const mainNavigationComponent = new MainNavigationComponent();
+const filterController = new FilterController(mainNavigationComponent.getElement(), cardsModel, onScreenChangeHandler);
+const filmsBoardComponent = new FilmsBoardComponent();
+const pageController = new PageController(filmsBoardComponent, cardsModel);
+const statisticsComponent = new StatisticsComponent(cardsModel);
+const filmsCountComponent = new FilmsCountComponent(cards.length);
+
+render(siteHeaderElement, profileComponent, RenderPosition.BEFOREEND);
 render(siteMainElement, mainNavigationComponent, RenderPosition.BEFOREEND);
 mainNavigationComponent.setActiveScreenChangeHandler(onScreenChangeHandler);
-
-// Render navigation menu
-const filterController = new FilterController(mainNavigationComponent.getElement(), cardsModel, onScreenChangeHandler);
 filterController.render();
-const watchedMovies = getCardsByFilter(FilterType.HISTORY, cardsModel.getCardsAll());
-
-// Render profile
-render(siteHeaderElement, new ProfileComponent(watchedMovies), RenderPosition.BEFOREEND);
-
-// Render films board
-const filmsBoardComponent = new FilmsBoardComponent();
 render(siteMainElement, filmsBoardComponent, RenderPosition.BEFOREEND);
-
-// Render all films lists
-const pageController = new PageController(filmsBoardComponent, cardsModel);
 pageController.render();
-
-// Render statistics
-const statisticsComponent = new StatisticsComponent(cardsModel);
 render(siteMainElement, statisticsComponent, RenderPosition.BEFOREEND);
 statisticsComponent.hide();
-
-// Render count of all movies
-const siteCountStatisticsElement = siteFooterElement.querySelector(`.footer__statistics`);
-render(siteCountStatisticsElement, new FilmsCountComponent(cards.length), RenderPosition.BEFOREEND);
+render(siteCountStatisticsElement, filmsCountComponent, RenderPosition.BEFOREEND);
